8a74901d3765b5617d33e561ca9045046275e295
hector.martineznu@elorrieta-errekamari.com <hector.martineznu@elorrieta-errekamari.com>
2024-04-22 08:22:51 +0200

    Menuaren bista amaituta

--- Erronka4Prog/src/Vista/vMenua.java
+++ Erronka4Prog/src/Vista/vMenua.java
@@ -1,5 +1,75 @@
 package Vista;
 
-public class vMenua {
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class vMenua extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					vMenua frame = new vMenua();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public vMenua() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Aukeratu");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(170, 11, 98, 26);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Atzera");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnNewButton.setBounds(10, 15, 107, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Nire Profila");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnNewButton_1.setBounds(317, 15, 107, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Musika deskubritu");
+		btnNewButton_2.setBounds(142, 72, 160, 23);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Podcast deskubritu");
+		btnNewButton_3.setBounds(142, 131, 160, 23);
+		contentPane.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("Nire PlayList-ak");
+		btnNewButton_4.setBounds(142, 183, 160, 23);
+		contentPane.add(btnNewButton_4);
+	}
 }
